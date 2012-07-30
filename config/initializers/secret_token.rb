7e6dc9ebb83608ee418ec1acc3b70bbffe90c844
Ken Shimizu <kenshimizu88@gmail.com>
2012-07-30 10:39:13 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollice::Application.config.secret_token = '597661bd510765c8456acb3da23ab22f57700f8cc49d6b67b8790fd81eb1eeb727ca485b7b52806d88ee1af0f6fec3c50011bb41f341deca79dcce2795d04d89'
